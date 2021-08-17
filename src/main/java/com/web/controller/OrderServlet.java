4f8ca11576ce05120d24b750fc73e0bbc21591dc
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-17 15:26:04 +0800

    Add files via upload

--- src/main/java/com/web/controller/OrderServlet.java
+++ src/main/java/com/web/controller/OrderServlet.java
@@ -31,11 +31,14 @@ public class OrderServlet extends HttpServlet {
 		int amount = Integer.parseInt(p_amount);
 		double discount = Double.parseDouble(p_discount);
 		double total = service.getTotal(price, amount, discount);
+		// 取得商品名稱
+		int id = Integer.parseInt(p_id);
+		String p_name = service.getNameById(id);
 		
 		// 分派(dispatcher)並重導(forward) 到 /WEB-INF/jsp/order_result.jsp
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/order_result.jsp");
 		// 設定要傳送到 jsp 的資料
-		req.setAttribute("p_id", p_id);
+		req.setAttribute("p_name", p_name);
 		req.setAttribute("total", total);
 		rd.forward(req, resp);
 		
--- src/main/java/com/web/service/OrderService.java
+++ src/main/java/com/web/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.web.service;
 
 public class OrderService {
+	private static String[] names = {"Water", "Coffee", "Milk", "Tea"}; 
 	
 	// 計算總金額
 	public double getTotal(int price, int amount, double discount) {
@@ -8,4 +9,9 @@ public class OrderService {
 		return total;
 	}
 	
+	// 根據 id 取 name
+	public String getNameById(int id) {
+		String name = names[id];
+		return name;
+	}
 }
