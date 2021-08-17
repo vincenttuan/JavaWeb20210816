fe85572caf690d86babce7035a17b35343ab3ca3
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-17 09:46:46 +0800

    Add files via upload

--- src/main/java/com/web/controller/CalcServlet.java
+++ src/main/java/com/web/controller/CalcServlet.java
@@ -1,11 +1,50 @@
 package com.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /*
  寫一個 CalcServlet 當	
  瀏覽器輸入 : http://localhost:8080/JavaWeb20210816/controller/calc?h=170&w=60
+ HTTP 方法 : GET
  網頁可以得到 : 20.76 
 */
 
-public class CalcServlet {
+@WebServlet(urlPatterns = {"/controller/calc"})
+public class CalcServlet extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String h = req.getParameter("h");
+		String w = req.getParameter("w");
+		PrintWriter out = resp.getWriter();
+		
+		// 檢查參數
+		if(h != null && w != null) {
+			try {
+				// 字串轉浮點數
+				double double_h = Double.parseDouble(h);
+				double double_w = Double.parseDouble(w);
+				double bmi = double_w / Math.pow(double_h/100, 2);
+				out.print(String.format("h: %.1f w: %.1f bmi: %.2f", double_h, double_w, bmi));
+			} catch (Exception e) {
+				// 轉換失敗
+				out.print("Arg is not numeric: " + e);
+			}
+		} else {
+			// 無設定參數
+			out.print("Args is null !");
+		}
+		
+		
+	}
+	
+	
 }
