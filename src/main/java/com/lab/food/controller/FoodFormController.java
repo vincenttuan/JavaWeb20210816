6da7cd4f21d1673d33ded3c0627c85d3b1a2dade
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-18 10:42:02 +0800

    Add files via upload

--- src/main/java/com/lab/food/controller/FoodFormController.java
+++ src/main/java/com/lab/food/controller/FoodFormController.java
@@ -1,5 +1,26 @@
 package com.lab.food.controller;
 
-public class FoodFormController {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lab.food.service.FoodService;
+
+@WebServlet(urlPatterns = "/lab/food/form")
+public class FoodFormController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		FoodService foodService = new FoodService();
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/lab/food/food_form.jsp");
+		rd.forward(req, resp);
+	}
+	
 }
