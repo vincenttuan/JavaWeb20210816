6cd13757f11445db270a56aa1c555d60914430bc
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-18 11:46:33 +0800

    Add files via upload

--- src/main/java/com/lab/food/service/FoodService.java
+++ src/main/java/com/lab/food/service/FoodService.java
@@ -1,5 +1,17 @@
 package com.lab.food.service;
 
-public class FoodService {
+import java.util.List;
 
+import com.lab.food.dao.Food;
+import com.lab.food.dao.FoodDao;
+
+public class FoodService {
+	
+	private FoodDao foodDao = new FoodDao();
+	
+	// 查詢所有庫存資料
+	public List<Food> queryFoods() {
+		return foodDao.findAll();
+	}
+	
 }
