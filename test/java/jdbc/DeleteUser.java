ee3532f1cd380b0429166d043c2adab768ae61c3
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-20 11:53:43 +0800

    Add files via upload

--- test/java/jdbc/DeleteUser.java
+++ test/java/jdbc/DeleteUser.java
@@ -4,9 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-import org.apache.commons.codec.binary.Base64;
-
-public class UpdateUser {
+public class DeleteUser {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
@@ -14,27 +12,21 @@ public class UpdateUser {
 		String username = "root";
 		String password = "12345678";
 		
-		String sql = "Update user set u_password=? where u_id=?";
+		// 請把 u_id = 1 的資料刪除
+		String sql = "Delete from user where u_id=?";
 		
 		try(Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
 			PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			
-			//----------------------------------------------------------
-			Base64 base64 = new Base64();
-			String pwd = "1234"; 
-			String pwd_base64 = base64.encodeAsString(pwd.getBytes());
-			System.out.printf("%s 編碼後變成 %s\n", pwd, pwd_base64);
-			//----------------------------------------------------------
-			pstmt.setString(1, pwd_base64);
-			pstmt.setInt(2, 99);
-			
+			int u_id = 1;
+			pstmt.setInt(1, u_id);
+				
 			int rowcount = pstmt.executeUpdate(); // 執行 sql 更新語句, 並回傳資料表對此 sql 產生的異動筆數
 			System.out.println("異動筆數: " + rowcount);
-			
+				
 		} catch (Exception e) {
-			e.printStackTrace(System.out);
+				e.printStackTrace(System.out);
 		}
-		
 	}
 
 }
